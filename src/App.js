fc77fabc558f4c8dff045bf0a29c6b106c9446f2
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-11-21 20:47:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from 'react';
 
 function App() {
+  const [accessibilityValue, setAccessibilityValue] = useState(0);
+  const [participantsValue, setParticipantsValue] = useState('');
+  const [priceValue, setPriceValue] = useState(0)
+
+  const handleAccessibilityChange = (event) => {
+    setAccessibilityValue(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setPriceValue(event.target.value);
+  };
+
+  const handleParticipantsChange = (event) => {
+    setParticipantsValue(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Get Unbored</h1>
+        <div className="search">
+          <input placeholder="accessibility, type, participants, price"></input>
+        </div>
+        <div className="filters">
+          <label htmlFor="accessibility">Accessibility:</label>
+          <input
+            type="range"
+            id="accessibility"
+            min="0"
+            max="1"
+            step="0.1"
+            value={accessibilityValue}
+            onChange={handleAccessibilityChange}
+          />
+          <div className="slider-value">
+            <p>Value: {accessibilityValue}</p>
+          </div>
+          <label for="type">Type:</label>
+          <select id="type">
+            <option value="">All</option>
+            <option value="education">Education</option>
+            <option value="recreational">Recreational</option>
+            <option value="social">SOcial</option>
+            <option value="diy">DIY</option>
+            <option value="charity">Charity</option>
+            <option value="cooking">Cooking</option>
+            <option value="relaxation">Relaxation</option>
+            <option value="music">Music</option>
+            <option value="busywork">Busywork</option>
+          </select>
+
+          <label htmlFor="participants">Participants:</label>
+          <input
+            type="number"
+            id="participants"
+            min="1"
+            value={participantsValue}
+            onChange={handleParticipantsChange}
+          />
+
+          <label htmlFor="price">Price:</label>
+          <input
+            type="range"
+            id="price"
+            min="0"
+            max="1"
+            step="0.05"
+            value={priceValue}
+            onChange={handlePriceChange}
+          />
+          <div className="slider-value">
+            <p>Value: {priceValue}</p>
+          </div>
+        </div>
       </header>
+
+      <div className="container">
+
+      </div>
     </div>
   );
 }
